fc14284426947e3fbf45ad0577e05f8b317cbf07
Jonathan Bleau Guerette <Jonathan.BGuerette@solotech.com>
2024-06-10 13:32:23 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Refine, WelcomePage } from "@refinedev/core";
+
+function App() {
+  return (
+    <Refine>
+      <WelcomePage />
+    </Refine>
+  );
+}
+
+export default App;
